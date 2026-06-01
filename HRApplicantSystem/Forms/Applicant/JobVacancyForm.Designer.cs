d9d297345ca81a07ae06d0f8f80a0462fbc476c7
Hans Caga-anan <cagaanan.hans@gmail.com>
2026-06-02 02:00:29 +0800

    Added JobVacancyForm along with its features

---
+++ HRApplicantSystem/Forms/Applicant/JobVacancyForm.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace HRApplicantSystem.Forms.Applicant
+{
+    partial class JobVacancyForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.pnlSearch = new System.Windows.Forms.Panel();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.cmbDepartment = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.dgvJobs = new System.Windows.Forms.DataGridView();
+            this.grpJobDetails = new System.Windows.Forms.GroupBox();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.lblDept = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnlSearch.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJobs)).BeginInit();
+            this.grpJobDetails.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlSearch
+            // 
+            this.pnlSearch.Controls.Add(this.btnReset);
+            this.pnlSearch.Controls.Add(this.btnSearch);
+            this.pnlSearch.Controls.Add(this.cmbDepartment);
+            this.pnlSearch.Controls.Add(this.txtSearch);
+            this.pnlSearch.Controls.Add(this.lblSearch);
+            this.pnlSearch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlSearch.Location = new System.Drawing.Point(0, 0);
+            this.pnlSearch.Name = "pnlSearch";
+            this.pnlSearch.Size = new System.Drawing.Size(900, 60);
+            this.pnlSearch.TabIndex = 0;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(540, 17);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(85, 25);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(445, 17);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(85, 25);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // cmbDepartment
+            // 
+            this.cmbDepartment.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbDepartment.FormattingEnabled = true;
+            this.cmbDepartment.Location = new System.Drawing.Point(280, 18);
+            this.cmbDepartment.Name = "cmbDepartment";
+            this.cmbDepartment.Size = new System.Drawing.Size(150, 21);
+            this.cmbDepartment.TabIndex = 2;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(90, 18);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(180, 20);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(15, 21);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(69, 13);
+            this.lblSearch.TabIndex = 0;
+            this.lblSearch.Text = "Search Jobs:";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 60);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.dgvJobs);
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(10);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.grpJobDetails);
+            this.splitContainer1.Panel2.Padding = new System.Windows.Forms.Padding(10);
+            this.splitContainer1.Size = new System.Drawing.Size(900, 490);
+            this.splitContainer1.SplitterDistance = 420;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // dgvJobs
+            // 
+            this.dgvJobs.AllowUserToAddRows = false;
+            this.dgvJobs.AllowUserToDeleteRows = false;
+            this.dgvJobs.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvJobs.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dgvJobs.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvJobs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvJobs.Location = new System.Drawing.Point(10, 10);
+            this.dgvJobs.MultiSelect = false;
+            this.dgvJobs.Name = "dgvJobs";
+            this.dgvJobs.ReadOnly = true;
+            this.dgvJobs.RowHeadersVisible = false;
+            this.dgvJobs.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvJobs.Size = new System.Drawing.Size(400, 470);
+            this.dgvJobs.TabIndex = 0;
+            this.dgvJobs.SelectionChanged += new System.EventHandler(this.dgvJobs_SelectionChanged);
+            // 
+            // grpJobDetails
+            // 
+            this.grpJobDetails.Controls.Add(this.txtDescription);
+            this.grpJobDetails.Controls.Add(this.lblDept);
+            this.grpJobDetails.Controls.Add(this.lblTitle);
+            this.grpJobDetails.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpJobDetails.Enabled = false;
+            this.grpJobDetails.Location = new System.Drawing.Point(10, 10);
+            this.grpJobDetails.Name = "grpJobDetails";
+            this.grpJobDetails.Size = new System.Drawing.Size(456, 470);
+            this.grpJobDetails.TabIndex = 0;
+            this.grpJobDetails.TabStop = false;
+            this.grpJobDetails.Text = "Job Specifications";
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDescription.BackColor = System.Drawing.SystemColors.Window;
+            this.txtDescription.Location = new System.Drawing.Point(20, 85);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.ReadOnly = true;
+            this.txtDescription.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDescription.Size = new System.Drawing.Size(416, 365);
+            this.txtDescription.TabIndex = 2;
+            // 
+            // lblDept
+            // 
+            this.lblDept.AutoSize = true;
+            this.lblDept.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDept.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.lblDept.Location = new System.Drawing.Point(17, 55);
+            this.lblDept.Name = "lblDept";
+            this.lblDept.Size = new System.Drawing.Size(75, 15);
+            this.lblDept.TabIndex = 1;
+            this.lblDept.Text = "Department:";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(16, 25);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(236, 20);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Select a Job to View Details";
+            // 
+            // JobVacancyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 550);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.pnlSearch);
+            this.Name = "JobVacancyForm";
+            this.Text = "Available Job Openings";
+            this.Load += new System.EventHandler(this.JobVacancyForm_Load);
+            this.pnlSearch.ResumeLayout(false);
+            this.pnlSearch.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvJobs)).EndInit();
+            this.grpJobDetails.ResumeLayout(false);
+            this.grpJobDetails.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlSearch;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ComboBox cmbDepartment;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.DataGridView dgvJobs;
+        private System.Windows.Forms.GroupBox grpJobDetails;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDept;
+        private System.Windows.Forms.TextBox txtDescription;
+    }
+}
---
+++ HRApplicantSystem/Forms/Applicant/JobVacancyForm.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+using HRApplicantSystem.Database; // Connects to your database helper namespace
+
+namespace HRApplicantSystem.Forms.Applicant
+{
+    public partial class JobVacancyForm : Form
+    {
+        private DataTable dtJobs = new DataTable();
+
+        public JobVacancyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void JobVacancyForm_Load(object sender, EventArgs e)
+        {
+            LoadDepartments();
+            LoadJobVacancies();
+        }
+
+        // Retrieves all open job vacancies from your database helper
+        private void LoadJobVacancies()
+        {
+            try
+            {
+                using (OleDbConnection conn = DBConnection.GetConnection())
+                {
+                    if (conn == null)
+                    {
+                        return; // DBConnection already warns the user if connection fails
+                    }
+
+                    // Query matches your exact columns: JobID, JobTitle, Department, Description, Status
+                    string query = "SELECT JobID, JobTitle, Department, Description, Status " +
+                                   "FROM JobVacancies WHERE Status = 'Open'";
+
+                    using (OleDbDataAdapter adapter = new OleDbDataAdapter(query, conn))
+                    {
+                        dtJobs.Clear();
+                        adapter.Fill(dtJobs);
+                        dgvJobs.DataSource = dtJobs;
+
+                        // Hide technical columns from display
+                        if (dgvJobs.Columns["JobID"] != null) dgvJobs.Columns["JobID"].Visible = false;
+                        if (dgvJobs.Columns["Description"] != null) dgvJobs.Columns["Description"].Visible = false;
+                        if (dgvJobs.Columns["Status"] != null) dgvJobs.Columns["Status"].Visible = false;
+
+                        // Customize visible column headers
+                        if (dgvJobs.Columns["JobTitle"] != null) dgvJobs.Columns["JobTitle"].HeaderText = "Job Title";
+                        if (dgvJobs.Columns["Department"] != null) dgvJobs.Columns["Department"].HeaderText = "Department";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading job vacancies: " + ex.Message, "Database Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Populates search dropdown filter with unique departments from active vacancies
+        private void LoadDepartments()
+        {
+            cmbDepartment.Items.Clear();
+            cmbDepartment.Items.Add("All Departments");
+
+            try
+            {
+                using (OleDbConnection conn = DBConnection.GetConnection())
+                {
+                    if (conn == null) return;
+
+                    string query = "SELECT DISTINCT Department FROM JobVacancies WHERE Status = 'Open'";
+                    using (OleDbCommand cmd = new OleDbCommand(query, conn))
+                    {
+                        conn.Open();
+                        using (OleDbDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string dept = reader["Department"]?.ToString();
+                                if (!string.IsNullOrEmpty(dept))
+                                {
+                                    cmbDepartment.Items.Add(dept);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                // Fallback options in case table contains no records yet
+                cmbDepartment.Items.Add("IT");
+                cmbDepartment.Items.Add("HR");
+                cmbDepartment.Items.Add("Finance");
+            }
+
+            cmbDepartment.SelectedIndex = 0;
+        }
+
+        // Logic for search filtering of the local grid data
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string keyword = txtSearch.Text.Trim().Replace("'", "''");
+            string selectedDept = cmbDepartment.SelectedItem?.ToString() ?? "All Departments";
+
+            DataView dv = dtJobs.DefaultView;
+            string filterExpr = "";
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                filterExpr = $"(JobTitle LIKE '%{keyword}%' OR Description LIKE '%{keyword}%')";
+            }
+
+            if (selectedDept != "All Departments")
+            {
+                if (!string.IsNullOrEmpty(filterExpr))
+                    filterExpr += " AND ";
+                filterExpr += $"Department = '{selectedDept}'";
+            }
+
+            dv.RowFilter = filterExpr;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtSearch.Clear();
+            if (cmbDepartment.Items.Count > 0)
+            {
+                cmbDepartment.SelectedIndex = 0;
+            }
+            LoadJobVacancies();
+        }
+
+        // Populates the detail viewing section when a grid row is clicked
+        private void dgvJobs_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvJobs.SelectedRows.Count > 0)
+            {
+                DataGridViewRow row = dgvJobs.SelectedRows[0];
+
+                lblTitle.Text = row.Cells["JobTitle"].Value?.ToString() ?? "";
+                lblDept.Text = "Department: " + (row.Cells["Department"].Value?.ToString() ?? "N/A");
+                txtDescription.Text = row.Cells["Description"].Value?.ToString() ?? "";
+
+                grpJobDetails.Enabled = true;
+            }
+            else
+            {
+                lblTitle.Text = "Select a Job to View Details";
+                lblDept.Text = "";
+                txtDescription.Clear();
+                grpJobDetails.Enabled = false;
+            }
+        }
+    }
+}
